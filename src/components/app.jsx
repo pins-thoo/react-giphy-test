16e13eb87ec0e1d4cfc28e18b05251e261c00660
Pins Thoo <aipeng.thoo@gmail.com>
2020-07-06 22:46:41 +0800

    Adding css class to App

---
+++ src/components/app.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="left-scene">Left</div>
+        <div className="right-scene">Right</div>
+      </div>
+    );
+  }
+}
+
+export default App;
